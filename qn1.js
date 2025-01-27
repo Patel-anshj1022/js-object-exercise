4687eee3d8bd26777711be62aff6f5139c2b0592
Patel-anshj1022 <patelanshj950@gmail.com>
2025-01-27 16:21:58 +0300

    Answerd qn1

---
+++ qn1.js
@@ -0,0 +1,15 @@
+const car = {
+    brand : 'Mercedes',
+    model : 'AMG'
+
+};
+
+//There are two correct syntax to alert Mercedes, name them
+
+//1) The bracket notation
+console.log(car['brand']);
+
+
+//2) THe dot notation
+console.log(car.brand);
+
