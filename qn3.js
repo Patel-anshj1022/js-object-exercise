344d04dca47c04947948034d5287a84e63cb0b5c
Patel-anshj1022 <patelanshj950@gmail.com>
2025-01-27 16:27:10 +0300

    add qn3

---
+++ qn3.js
@@ -0,0 +1,11 @@
+const person = {
+    firstname: 'Ansh',
+    lastname: 'Patel',
+    fullname: function() {
+      return this.firstname + ' ' + this.lastname;
+    }
+  };
+  //How many methods does the object have?, Name them?
+  //Answer -
+  //The fullname method
+  
