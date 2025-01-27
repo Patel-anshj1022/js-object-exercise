9d67aa436ec69541af52e5f64d32ae2bdb52fd19
Patel-anshj1022 <patelanshj950@gmail.com>
2025-01-27 16:25:45 +0300

    add qn2

---
+++ qn2.js
@@ -0,0 +1,14 @@
+const person = {
+    firstname: 'Jane',
+    lastname: 'Doe',
+    fullname: function() {
+        return this.firstname + '' + this.lastname;
+    }
+    };
+
+//How many properties does the object have? Name them
+//Answer -
+ //3
+ //The firstname
+ //The lastname
+ //The fullname
